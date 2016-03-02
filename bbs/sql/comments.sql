e97eb5871caa420822ed401e30dbfcde28e74acb
mizutani <y_mizutani@ic-com.co.jp>
2016-03-02 11:15:22 +0900

    sql

---
+++ bbs/sql/comments.sql
@@ -0,0 +1,8 @@
+CREATE TABLE comments(
+ message_id INT(11) NOT NULL
+ comment_text TEXT NOT NULL
+ ,insert_date TIMESTAMP NOT NULL
+ ,update_date TIMESTAMP NOT NULL
+ ,user_name VARCHAR(255)
+ )
+ 
---
+++ bbs/sql/users.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users(
+ id INT(11) PRIMARY KEY auto_increment
+ ,login_id VARCHAR(255) UNIQUE NOT NULL
+ ,password VARCHAR(255) NOT NULL
+ ,name VARCHAR(255) NOT NULL
+ ,branch_id INT(11) NOT NULL
+ ,department_id INT(11) NOT NULL
+ ,user_state INT(11) DEFAULT 0
+ ,insert_date TIMESTAMP NOT NULL
+ ,update_date TIMESTAMP NOT NULL
+ )
