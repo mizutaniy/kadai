0f889082aa46d8182568c7824563c9ca5d13cc71
mizutani <y_mizutani@ic-com.co.jp>
2016-03-02 18:45:33 +0900

    add ","

--- bbs/sql/user_comment.sql
+++ bbs/sql/user_comment.sql
@@ -1,10 +1,10 @@
 CREATE VIEW user_comment AS
- SELECT
- message_id
-  text
-  ,name
-  ,comments.insert_date
- FROM
-  users, comments
- WHERE
-  users.id = comments.user_id
+  SELECT
+  message_id
+   ,text
+   ,name
+   ,comments.insert_date
+  FROM
+   users, comments
+  WHERE
+   users.id = comments.user_id
