114c06cd1031f15b250b6a4365b50dfe72993c1f
mizutani <y_mizutani@ic-com.co.jp>
2016-03-02 13:56:13 +0900

    users column chaged name to user_name

--- bbs/sql/users.sql
+++ bbs/sql/users.sql
@@ -2,7 +2,7 @@ CREATE TABLE users(
  id INT(11) PRIMARY KEY auto_increment
  ,login_id VARCHAR(255) UNIQUE NOT NULL
  ,password VARCHAR(255) NOT NULL
- ,name VARCHAR(255) NOT NULL
+ ,user_name VARCHAR(255) NOT NULL
  ,branch_id INT(11) NOT NULL
  ,department_id INT(11) NOT NULL
  ,user_state INT(11) DEFAULT 0
