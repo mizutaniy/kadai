3e87c27f62a1f77c821108898584731fc80d89cf
mizutani <y_mizutani@ic-com.co.jp>
2016-03-02 17:49:02 +0900

    create view

--- bbs/sql/comments.sql
+++ bbs/sql/comments.sql
@@ -2,7 +2,7 @@ CREATE TABLE comments(
 id INT(11) PRIMARY KEY auto_increment
  ,user_id INT(11) NOT NULL
  ,message_id INT(11) NOT NULL
- ,comment_text TEXT NOT NULL
+ ,text TEXT NOT NULL
  ,insert_date TIMESTAMP NOT NULL
  ,update_date TIMESTAMP NOT NULL
  )
--- bbs/sql/messages.sql
+++ bbs/sql/messages.sql
@@ -2,7 +2,7 @@ CREATE TABLE messages(
  id INT(11) PRIMARY KEY auto_increment
  ,user_id INT(11) NOT NULL
  ,title VARCHAR(255) NOT NULL
- ,message_text TEXT NOT NULL
+ ,text TEXT NOT NULL
  ,category VARCHAR(255) NOT NULL
  ,insert_date TIMESTAMP NOT NULL
  ,update_date TIMESTAMP NOT NULL
