3db54edc3811c6373e2eba9d8671f7eaac927b3f
mizutani <y_mizutani@ic-com.co.jp>
2016-03-02 11:14:01 +0900

    sql

--- bbs/sql/comment.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE comment(
- main_text TEXT NOT NULL
- ,insert_date TIMESTAMP NOT NULL
- ,update_date TIMESTAMP NOT NULL
- ,user_name VARCHAR(255)
- )
- 
- alter table comment change main_text message text not null
- 
--- bbs/sql/post.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE post(
- id INT(11) PRIMARY KEY auto_increment
- ,post_name VARCHAR(255) UNIQUE NOT NULL
- )
